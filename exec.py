f936a15871fdd9bbb52271713ad02b376f96ed3a
Kenryo98 <ledeunff.adrien@gmail.com>
2016-05-03 14:31:15 +0200

    fullscreen

--- exec.py
+++ exec.py
@@ -55,8 +55,8 @@ class Boutons :
 
 
 root = Tk()
-root.grid_bbox(0,0,1,1)
-root.geometry("800x480+0+0")
+root.attributes("-fullscreen", 1)
+#root.geometry("800x480+0+0")
 root.resizable(width=False,height=False)
 police=tkFont.Font(root, size=16, family='Courier')
 policeBouton=tkFont.Font(root, size=14, family='Courier')
